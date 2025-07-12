3cb6e683a5dfdd9a5406d6ffe2f9e74ff09104fb
Juan Pablo Leon <fonseca123papo@gmail.com>
2025-07-12 17:04:00 -0500

    actualizando el enrutado

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,12 @@ import { Login } from './ui/pages/Login/Login.tsx'
 import { HashRouter, Routes, Route } from 'react-router-dom'
 import { storeGlobal } from './domain/statesGlobal/storeGlobal.ts'
 import { Provider } from 'react-redux'
-import { BASE_URL } from './domain/models/envirments.ts'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={storeGlobal}>
-      <HashRouter basename={BASE_URL}>
+      <HashRouter>
         <main className='main'>
           <Routes>
             <Route path="/" element={<Login />} />
